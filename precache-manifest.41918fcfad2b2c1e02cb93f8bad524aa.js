907534b69a2837bce821eb5f231ddc2780ba0b33
lijun <1094453726@qq.com>
2022-09-03 16:51:59 +0800

    deploy

---
+++ precache-manifest.41918fcfad2b2c1e02cb93f8bad524aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d496a2b837fd751f322",
+    "url": "/\n\nmorney-3-website/css/app.f12b5f79.css"
+  },
+  {
+    "revision": "02f89a67881c8e404d292a3eb13da0d3",
+    "url": "/\n\nmorney-3-website/index.html"
+  },
+  {
+    "revision": "2d496a2b837fd751f322",
+    "url": "/\n\nmorney-3-website/js/app.2a5887b8.js"
+  },
+  {
+    "revision": "679be4ff3987458f6ecf",
+    "url": "/\n\nmorney-3-website/js/chunk-vendors.4e7f0c29.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/\n\nmorney-3-website/manifest.json"
+  },
+  {
+    "revision": "7f29fca7d2d9fb9234852d81ce962848",
+    "url": "/\n\nmorney-3-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/\n\nmorney-3-website/robots.txt"
+  }
+]);
